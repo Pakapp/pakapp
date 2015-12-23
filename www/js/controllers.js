63103dd3db368e3e265fdd6d951afcb1862a091d
cholathit <kcholathit@gmail.com>
2015-12-23 21:45:49 +0700

    fix auth id discrepancy

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,68 +1,5 @@
 angular.module('app')
 
-.controller('appCtrl', function($scope, $timeout, $state, Users, $filter, Auth, Farms, Packages, profile, FirebaseUrl) {
-
-  var appCtrl = this;
-  $scope.goto = function(state){
-      $state.go(state);
-  };
-
-  $scope.users  = Users;
-  $scope.auth   = Auth;
-  $scope.packages = Packages;
-  $scope.farms = Farms;
-  $scope.profile = profile;
-
-  console.log($scope.packages);
-
-  console.log($scope.profile);
-
-  $scope.user = {
-          email: '',
-          password: '',
-          firstname:'',
-          lastname:'',
-          b_day:'',
-          b_month:'',
-          b_year:''
-        }
-  //Register with facebook
-    $scope.registerFB = function(){
-      $scope.auth.ref.authWithOAuthPopup("facebook", function(error, authData) {
-        if (error) {
-          console.log("Login Failed!", error);
-        } else {
-          $scope.users.userRef.child('fb'+authData.facebook.id).set({
-            firstname:        authData.facebook.cachedUserProfile.first_name,
-            lastname:         authData.facebook.cachedUserProfile.last_name,
-            profile_image:    authData.facebook.profileImageURL,
-            gender:           authData.facebook.cachedUserProfile.gender,
-            date_of_birth:    authData.facebook.cachedUserProfile.birthday,
-            locale:           authData.facebook.cachedUserProfile.locale,
-            fb_locationID:    authData.facebook.cachedUserProfile.location.id,
-            fb_location_name: authData.facebook.cachedUserProfile.location.name,
-            join:             $filter('date')(new Date(), 'yyyy-MM-dd'),
-          });
-          $state.go('home');
-        }
-      }, {
-        scope: "email,user_likes,user_birthday,user_location" // the permissions requested
-      });
-    }
-
-    $scope.subscribe = function(pid){
-      $scope.users.userRef.child($scope.profile.$id).child('subscriptions').child(pid).set(true);
-    }
-
-
-    $scope.logout = function(){
-      var ref = new Firebase(FirebaseUrl);
-      ref.unauth();
-      $state.go('login', null, {reload: true});
-    }
-
-})
-
 .controller('authCtrl', function($scope, $timeout, $state, Users, $filter, Auth) {
 
     $scope.users  = Users;
@@ -74,7 +11,7 @@ angular.module('app')
         if (error) {
           console.log("Login Failed!", error);
         } else {
-          $scope.users.userRef.child('fb'+authData.facebook.id).set({
+          $scope.users.userRef.child('facebook:'+authData.facebook.id).set({
             firstname:        authData.facebook.cachedUserProfile.first_name,
             lastname:         authData.facebook.cachedUserProfile.last_name,
             profile_image:    authData.facebook.profileImageURL,
@@ -93,3 +30,36 @@ angular.module('app')
     }
 
 })
+
+
+.controller('appCtrl', function($scope, $timeout, $state, Users, $filter, Auth, Farms, Packages, profile, FirebaseUrl) {
+
+  var appCtrl = this;
+  $scope.goto = function(state){
+      $state.go(state);
+  };
+
+  $scope.users  = Users;
+  $scope.auth   = Auth;
+  $scope.packages = Packages;
+  $scope.farms = Farms;
+  $scope.profile = profile;
+
+  console.log($scope.packages);
+
+  console.log($scope.profile);
+
+
+    $scope.subscribe = function(pid){
+      $scope.users.userRef.child($scope.profile.$id).child('subscriptions').child(pid).set(true);
+    }
+
+
+    $scope.logout = function(){
+      var ref = new Firebase(FirebaseUrl);
+      ref.unauth();
+      $state.go('login', null, {reload: true});
+    }
+
+})
+
